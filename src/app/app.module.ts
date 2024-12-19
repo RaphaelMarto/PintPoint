6f8aefb0b081c6db70301a80c3c355a1a024111c
Raphaël Marto <divinitinatsu@outlook.fr>
2024-12-19 22:35:29 +0100

    [Modified] Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import { authInterceptor } from '../interceptor/auth-interceptor.interceptor';
+import { ButtonModule } from 'primeng/button';
 
 registerLocaleData(localeFr);
 
@@ -20,6 +21,7 @@ registerLocaleData(localeFr);
     BrowserAnimationsModule,
     AppRoutingModule,
     ShareModule,
+    ButtonModule
   ],
   providers: [provideHttpClient(withInterceptors([authInterceptor])),{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
